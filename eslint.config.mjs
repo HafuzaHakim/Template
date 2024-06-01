1af043308dd45f83865d69fa12376c89b72e1ef6
HafuzaHakim <hafuzahakim@gmail.com>
2024-06-02 01:28:36 +0800

    Added Eslint and Prettier configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,4 @@
+import config from 'eslint-config-airbnb';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default [config, eslintConfigPrettier];
---
+++ src/index.js
@@ -0,0 +1,1 @@
+const straw = 'this is a string';
